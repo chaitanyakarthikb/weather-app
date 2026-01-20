310e99301eebf482b0b0707c9729e58fe32b1578
Chaitanya Karthik <chaitanyakarthikbasva@gmail.com>
2026-01-20 11:32:55 +0530

    Implement location suggestions feature and also location selection feature

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchBar.css";
 import { useDarkModeContext } from "../store/DarkModeContext";
+import LocationSuggestion from "./LocationSuggestion";
 const SearchBar = () => {
-    const {darkMode,setDarkMode} = useDarkModeContext();
+  const { darkMode, setDarkMode } = useDarkModeContext();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedLocation,setSelectedLocation] = useState(null);
+
+  useEffect(()=>{
+    console.log("============selectedLocation",selectedLocation)
+
+  },[selectedLocation])
+
+  useEffect(() => {
+    const timeout = setTimeout(async () => {
+      if (searchQuery.length > 2) {
+        try {
+          setLoading(true);
+          let locationApi =
+            process.env.REACT_APP_PLACES_API +
+            `?q=${searchQuery}&limit=10&osm_tag=place:city`;
+
+          let response = await fetch(locationApi);
+          let data = await response.json();
+          console.log("=====data", data);
+          setSuggestions(data.features);
+          setLoading(false);
+        } catch (error) {
+          setError(error);
+          setLoading(false);
+          console.log("======error", error);
+        }
+      }
+    }, 300);
+    return () => clearInterval(timeout);
+  }, [searchQuery]);
   return (
     <div className="searchBar">
-      <input placeholder="Search City..." />
+      <div>
+        <input
+          type="text"
+          value={selectedLocation?.name || searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search City..."
+        />
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>Error...</h1>}
+        {suggestions &&
+         !selectedLocation &&
+          suggestions.map((el) => {
+            return <LocationSuggestion setSelectedLocation={setSelectedLocation} suggestionName={el?.properties?.name} coordinates={el?.geometry?.coordinates} suggestionCountry={el?.properties?.country} />;
+          })}
+      </div>
       <div className="toggle-switch">
         <label className="switch">
-          <input type="checkbox" onChange={()=>setDarkMode((prev)=>!prev)} checked={darkMode} />
+          <input
+            type="checkbox"
+            onChange={() => setDarkMode((prev) => !prev)}
+            checked={darkMode}
+          />
           <span className="slider round"></span>
         </label>
       </div>
